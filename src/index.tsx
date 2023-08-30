35b5953c5b3e36491bae2c056ed33c9301da8b01
marvy896 <onyex896@gmail.com>
2023-08-30 22:47:43 +0100

    init setup

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import {createRoot} from "react-dom/client"
+window.React = React
+import App from "./app"
+import "./index.css";
+import {BrowserRouter} from "react-router-dom"
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = createRoot(document.getElementById("root") as HTMLDivElement)
+root.render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+    )
