0e760a6ce42c1f6a8f7b2fe40ba98d96b63c4c63
marvy896 <onyex896@gmail.com>
2023-08-30 23:23:24 +0100

    Created Routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './screens/home'
+import Feed from './screens/feed'
+import Library from './screens/library'
+import Player from './screens/player'
 
 export default function App() {
   return (
     <div>
-      <h1>HELLO</h1>
+        <Routes>
+            <Route path='/home' element={<Home/>} />
+            <Route path='/feed' element={<Feed/>}/>
+            <Route path='/library' element={<Library/>} />
+            <Route path='/player' element={<Player/>} />
+            <Route path='/' element={<Home/>}/>
+        </Routes>
     </div>
   )
 }
