5a3c64bf4108417ebba6c9882853406cafb2f7a4
marvy896 <onyex896@gmail.com>
2023-09-28 22:56:55 +0100

    The Librart playlist is showing

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "../../components/sidebar/sidebar";
 import Feed from "../feed/feed";
@@ -7,11 +7,36 @@ import Player from "../player/player";
 import Favourites from "../favourires/favourites";
 import Trending from "../trending/trending";
 import './home.css'
+import Login from "../auth/login";
+import { setClientToken } from "../../spotify";
 
 export default function Home() {
-  return (
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash = "";
+    if (!token && hash) {
+      const _token = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token", _token);
+      setToken(_token);
+      setClientToken(_token);
+    } else {
+      if (token !== null){
+        setToken(token);
+        setClientToken(token);
+      }
+    }
+  }, []);
+
+  return !token ? (
+    <Login />
+  ) : (
     <Router>
       <div className="main-Home">
+       
         <Sidebar />
         <Routes>
           <Route path="/" element={<Library />} />
@@ -24,3 +49,5 @@ export default function Home() {
     </Router>
   );
 }
+
+
---
+++ src/spotify.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const authEndpoint = "https://accounts.spotify.com/authorize?";
+const clientId = "11d6ebeb8d314c90a39234b04f2e66c7";
+const redirectUri = "http://localhost:1234";
+const scopes = ["user-library-read", "playlist-read-private"];
+
+export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
+
+const apiClient = axios.create({
+  baseURL: "https://api.spotify.com/v1/",
+});
+
+export const setClientToken = (token) => {
+  apiClient.interceptors.request.use(async function (config) {
+    config.headers.Authorization = "Bearer " + token;
+    return config;
+  });
+};
+
+export default apiClient;
